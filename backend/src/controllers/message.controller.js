048174a5dc980fc2ebc541f4cb14d80d8b8944bd
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-27 13:37:27 +0300

    message conroller function

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -29,6 +30,37 @@ export const getMessages = async(req, res) => {
         res.status(200).json(messages)
     } catch (error) {
         console.log("Error in getMessages:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
+export const sendMessage = async (req,res) =>{
+    try {
+        const { text, image} = req.body;
+        const { id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image) {
+            //upload base6 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+        await newMessage.save();
+
+        // =====real time functionality====== //
+
+        res.status(201).json(newMessage)
+    }catch(error) {
+        console.log("Error in sendMessage:", error.message);
         res.status(500).json({ message: "Internal server error" });
     }
 }
