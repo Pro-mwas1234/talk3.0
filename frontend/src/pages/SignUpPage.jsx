8bc00051de7a13377c81ec699e0c9e2f7efab806
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-28 12:29:34 +0300

    signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore';
-import { Mail, MessageSquare, User } from 'lucide-react'
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from 'lucide-react'
 
 const SignUpPage = () => {
 
@@ -82,6 +82,49 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
+
+            <div className="form-control">
+              <label className='label'>
+                <span className='label-text font-medium'>
+                 Password
+                </span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className='size-5 text-base-content/40'/>
+                </div>
+                <input
+                type={showPassword ? "text" : "password"}
+                className={`input input-bordered w-full pl-10`}
+                placeholder='••••••••'
+                value={formData.password}
+                onChange={(e) => setFormData({...formData, password: e.target.value})}
+                />
+                <button
+                type='button' 
+                className="absolute inset-y-0 pr-3 right-0 flex items-center"
+                onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+
+           <button type='submit' className="btn btn-primary w-full " disabled={isSigningUp}>
+            {isSigningUp ? (
+              <>
+                <Loader2 className='size-5 animate-spin' /> Loading...
+              </>
+            ) : (
+              "Create Account"
+            )}
+           </button>
+
           </form>
 
         </div>
