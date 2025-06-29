3965671cccd3afcab90de4c9952b05c875206c73
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-29 15:38:43 +0300

    sidebar skeleton

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
+import SidebarSkeleton from './skeletons/SidebarSkeleton';
 
 const Sidebar = () => {
+
+    const { users, isUsersLoading, getUsers, selectedUser, setSelectedUser } = useChatStore();
+
+    const onlineUsers = [];
+
+    useEffect(() => {
+        getUsers();
+    }, [getUsers]);
+
+    if(isUsersLoading) return <SidebarSkeleton/>
+
   return (
     <div>Sidebar</div>
   )
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -33,5 +33,9 @@ export const useChatStore = create((set) => ({
         }finally{
             set({ isMessagesLoading: false })
         }
-    }
+    },
+
+    setSelectedUser: (selectedUser) => {
+        set({ selectedUser });
+    },
 }))
